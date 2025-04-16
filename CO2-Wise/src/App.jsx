9f2b6d7612959ecbebe1381756778288f2638cab
Pritam Sarkar <pritam7796419792@gmail.com>
2025-04-16 12:54:41 +0530

    carbom calculator ui done

--- CO2-Wise/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CarbonFootprintCalculator from "./components/calculator/CarbonFootprintCalculator";
-
-const App = () => {
-  return <CarbonFootprintCalculator />;
-};
-
-export default App;
